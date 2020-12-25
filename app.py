04a3b006cf39365744d16edc3446dd1169c56d02
ronith raj <ronithrajkopperla@gmail.com>
2020-12-25 12:11:30 +0530

    Changed the port in app.py

--- app.py
+++ app.py
@@ -307,8 +307,7 @@ def predict():
 
     return render_template("home.html")
 
-
-
-
 if __name__ == "__main__":
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
