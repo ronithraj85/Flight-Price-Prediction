f3a06664aa024c6b3695c2ec43450987da9c08d6
ronith raj <ronithrajkopperla@gmail.com>
2020-12-26 12:36:01 +0530

    Added date changes

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,11 @@
-var today = new Date().toISOString();
-document.getElementById("Dep_Time").min = today;
+var iso = new Date().toISOString();
+var minDate = iso.substring(0, iso.length - 8);
+document.getElementById("Arrival_Time").value = minDate
+document.getElementById("Arrival_Time").min = minDate
+
+
+
+var iso = new Date().toISOString();
+var minDate = iso.substring(0, iso.length - 8);
+document.getElementById("Dept_Time").value = minDate
+document.getElementById("Dept_Time").min = minDate
